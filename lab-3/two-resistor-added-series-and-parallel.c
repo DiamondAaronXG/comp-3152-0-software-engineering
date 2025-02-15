1c8b8b711bc392ddc3259df5fc3f0a1a558e3a22
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-02-14 18:05:44 -0800

    Update two-resistor-added-series-and-parallel.c
    added comments

--- lab-3/two-resistor-added-series-and-parallel.c
+++ lab-3/two-resistor-added-series-and-parallel.c
@@ -1,10 +1,12 @@
-#include <stdio.h> 
-
-  
+#include <stdio.h>   
 
 int main() { 
 
+    // Declares variables
+  
     float R1 , R2 , RS , RP ; 
+
+    // Asks for and scans user input
 
     printf("Enter R1 value: "); 
     scanf("%f" , &R1); 
@@ -12,8 +14,15 @@ int main() {
     printf("Enter R2 value: "); 
     scanf("%f" , &R2); 
 
+    // Calculates resistors in series
+
     RS = (R1 + R2); 
+
+    // Calcukates resistors in parrallel
+  
     RP = (R1*R2)/(R1 + R2); 
+
+    // Prints results
 
     printf("Series = %f\n" , RS); 
     printf("Parallel = %f" , RP); 
