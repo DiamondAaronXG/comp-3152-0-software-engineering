c12338fc9899748dc12c9e304c4cce5acc61f106
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-02-14 23:17:50 -0800

    Update calculator-using-switch-function.c
    added comments

--- lab-5/calculator-using-switch-function.c
+++ lab-5/calculator-using-switch-function.c
@@ -1,10 +1,15 @@
 #include <stdio.h>  
 
 int main() { 
+
+// Declares variables
+    
 double firstnumber; 
 double secondnumber; 
 double answer; 
 char operation;  
+
+// Asks for and scans user input
 
 printf("Enter First Number: "); 
 scanf("\n%lf", &firstnumber);  
@@ -15,19 +20,29 @@ scanf("\n%lf", &secondnumber);
 printf("Enter Operation Type (+,-,/,*): "); 
 scanf("\n%c", &operation);  
 
+// Switch case to caluclate using chosen operation
+
 switch(operation){  
+
+// Multiplies firstnumber and secondnumber
 
 case '*': answer = firstnumber * secondnumber ;  
     printf("answer: %lf\n", answer);  
     break; 
+
+// Divides firstnumber and secondnumber
      
 case '/': answer = firstnumber / secondnumber ; 
     printf("answer: %lf\n", answer); 
     break; 
+
+// Subtracts secondnumber from firstnumber
      
 case '-': answer = firstnumber - secondnumber ; 
     printf("answer: %lf\n", answer); 
     break; 
+
+// Adds secondnumber to firstnumber
      
 case '+': answer = firstnumber + secondnumber ; 
     printf("answer: %lf\n", answer); 
