94776b650e353ede4208114f347b368397cc2985
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-02-14 23:50:58 -0800

    Update sine-of-x.c
    added comments
    
    changed variable names
    
    added an option for user input rather than set values
    
    changes output variable names

--- lab-2/sine-of-x.c
+++ lab-2/sine-of-x.c
@@ -1,16 +1,31 @@
-#include < stdio.h>
-#define PI, 3.141592653
+#include <stdio.h>
+#define PI 3.141592653
 
 int main()
 {
 
-  int xdeg - 45;
-  double xr = (PI/180) xdeg;
-  double sum = 0;
+  // Declares variables
 
-  sum = (xr-((xr*xr*xr)/6)+((xr*xr*xr**xr*xr)/120)-((xr*xr*xr*xr*xr*xr*xr)/5040));
+  int xdeg ;
+  double xrad ;
+  double radestimation ;
 
-  printf(" xdeg = %i, xr %1f , sum %1f*, xdeg, xr, sum);
+  // Asks for and scans user input
+  
+  printf("Enter a value for x in degress: ");
+  scanf("%i", &xdeg);
+
+  // Calculates the radian value of the given degree value
+
+  xrad = (PI/180) * xdeg ;
+
+  // Estimates a radian value of the given degree value using Taylor Series
+  
+  radestimation = (xrad-((xrad*xrad*xrad)/6)+((xrad*xrad*xrad*xrad*xrad)/120)-((xrad*xrad*xrad*xrad*xrad*xrad*xrad)/5040));
+
+  // Prints results
+  
+  printf("x in degrees = %i, x in radians = %lf , radian estimation = %lf" , xdeg, xrad, radestimation);
 
   return 0;
 
