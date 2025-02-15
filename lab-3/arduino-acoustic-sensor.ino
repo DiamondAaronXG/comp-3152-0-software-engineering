abb3c1dc5a5a1fe9316bc117e6fabd4eaa3e207e
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-02-14 17:40:56 -0800

    Update arduino-acoustic-sensor.ino
    added better comments

--- lab-3/arduino-acoustic-sensor.ino
+++ lab-3/arduino-acoustic-sensor.ino
@@ -1,9 +1,9 @@
 #define PIN_TRIG 3 
 #define PIN_ECHO 2 
 
-void setup() { 
+// Sets the mode for each pin being used
 
- //sets the mode for each pin being used
+void setup() { 
  
   pinMode(13, OUTPUT); 
   Serial.begin(115200); 
@@ -12,7 +12,7 @@ void setup() {
 
 } 
 
- 
+// Sets the environment 
 
 void loop() { 
 
@@ -27,16 +27,27 @@ void loop() {
   // Read the result: 
 
   int duration = pulseIn(PIN_ECHO, HIGH); 
+
+  // Tells the arduino when each led should be on
+ 
   int led_valuer = (duration / 58)>50; 
   int led_valuey = (duration / 58)>=10 && (duration / 58)<=50; 
   int led_valueg = (duration / 58)<10; 
+
+  // Tells the arduino which LED is connected to each pin
+ 
   digitalWrite(11, led_valuer); 
   digitalWrite(12, led_valuey); 
   digitalWrite(13, led_valueg); 
+
+   // Prints the output of the distance sensor in centimeters and inches
+ 
   Serial.print("Distance in CM: "); 
   Serial.println(duration / 58); 
   Serial.print("Distance in inches: "); 
   Serial.println(duration / 148); 
+
+  // Time between sensor detections
 
   delay(1000); 
 
