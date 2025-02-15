ed9bd3d7b654dd607f6036d8f06ad075df615e90
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-02-14 22:50:34 -0800

    Update sum-of-first-n-even-integers.c
    added comments

--- lab-4/sum-of-first-n-even-integers.c
+++ lab-4/sum-of-first-n-even-integers.c
@@ -3,20 +3,31 @@
 int main() 
 { 
 
+  // Declaring variables
+
   int n; 
   int sum = 0; 
   int counter = 2; 
 
+  // Asks for and scans user input
+
   printf("Enter n:"); 
   scanf("%d", &n); 
+
+  // For counter equals 1, if counter is greater than n, add 1 to the counter
 
   for (counter = 1; counter <=n; counter++) 
 
     { 
+
+      //sum equals sum + (2 times counter)
     
       sum += 2*counter; 
     
     } 
+
+  // Print results
+  
   printf("\n sum = %d\n", sum); 
 
   return 0; 
