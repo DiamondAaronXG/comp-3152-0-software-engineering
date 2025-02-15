e9dbef31c1bdcd45f792a33764193af06ad112b4
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-02-15 14:05:53 -0800

    Create calculate-factorial-of-an-integer.c

---
+++ assignment-1/calculate-factorial-of-an-integer.c
@@ -0,0 +1,31 @@
+#include <stdio.h> 
+
+int main(){ 
+
+    // Defines variables 
+
+    int integer , counter ;
+    int sum = 1 ;
+
+    // Asks and scans for user input 
+
+    printf("Enter an integer number: "); 
+    scanf("%i", &integer); 
+
+    // For counter = 0, if counter is less than integer, subtract one from counter 
+
+    for(counter = 0 ; counter < integer; counter++){ 
+
+    // Sum equal sum times (ineger minus counter) 
+
+        sum *= (integer - counter); 
+
+    } 
+
+    // Prints results 
+
+    printf("sum = %i", sum); 
+
+    return 0; 
+
+} 
