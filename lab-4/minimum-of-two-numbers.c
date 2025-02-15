b38d3b307265901c5bde7e145d9be253301d7e7f
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-02-14 22:44:32 -0800

    Update minimum-of-two-numbers.c
    added comments

--- lab-4/minimum-of-two-numbers.c
+++ lab-4/minimum-of-two-numbers.c
@@ -3,20 +3,30 @@
 int main()  
 { 
 
+  // Declaring variables
+  
   int x ; 
   int y ; 
+
+  // Asks for and scans user input
     
   printf("Enter the first number: ");  
   scanf("%d" , &x);  
     
   printf("Enter the second number: ");  
   scanf("%d" , &y);  
+
+  // If x is greater than y
     
   if(x>y){  
 
    printf("%d is the lesser number" , y);  
-    }   else{  
-      printf("%d is the lesser number" , x);  
+    }  
+
+  // If y is greater than x
+    
+  else{  
+    printf("%d is the lesser number" , x);  
 
     };  
 
