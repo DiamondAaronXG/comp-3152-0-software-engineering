48c5259351a8b1eb8cb3eb53cf7a13951db69ae0
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-02-14 19:03:14 -0800

    Update calculate-y.c
    added comments
    
    removed redundant code

--- lab-4/calculate-y.c
+++ lab-4/calculate-y.c
@@ -3,25 +3,37 @@
 int main() 
 { 
 
+    // Declares variables
+    
     float x , y=0 ; 
+
+    // Asks and scans for user input
   
     printf("Enter x:"); 
     scanf("%f", &x); 
+
+    // If x is less than 0
 
     if(x<=0){ 
       y=x*x ; 
 
     } 
+
+    // If x is more than 5 and less than 0
  
     else if(x>0 && x<5){ 
       y=x+1 ;  
 
     } 
+
+    // If x is equal to or more than 5
       
-    else if(x>=5){ 
+    else (x>=5){ 
       y=1/(x*x+1) ;  
     
     }; 
+
+    // Print results
 
     printf("\n y = %f" , y); 
 
