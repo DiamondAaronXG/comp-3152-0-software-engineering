2949bd7e78b2a775eac20adef9243b20ff466dd1
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-02-21 13:47:27 -0800

    Update fahrenheit-to-celsius-without-return-function.c
    added comments
    aded return 0

--- lab-6/fahrenheit-to-celsius-without-return-function.c
+++ lab-6/fahrenheit-to-celsius-without-return-function.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 
+// Declares function
+
 void temp(float F) {
+
+	// Prints "(F) degress in Celsius is (C) degrees" using the formula (F-32)*(5.0/9.0)
 
 	printf("%f degrees in Celsius is %f degrees", F, (F - 32) * (5.0 / 9.0));
 
 }
 int main() {
 
+	// Declares variables
+
 	float F ;
 	float C ;
 
+	// Asks and scans for user input
+
 	printf("Enter a Fahrenheit value:");
 	scanf("%f", &F);
+
+	// Calls temp function
+
 	temp(F);
 
+	return 0
 
 }
