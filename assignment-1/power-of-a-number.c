38f182b58ad707bd221d35fa9746d49ac870c6b1
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-02-20 16:42:05 -0800

    Update power-of-a-number.c
    added case for if power equal zero
    added case for if power is a negative integer

--- assignment-1/power-of-a-number.c
+++ assignment-1/power-of-a-number.c
@@ -1,35 +1,60 @@
 #include <stdio.h>
 
-int main(){
-    
-    // Defines variables
-    
-    double nonzero ;
-    double sum = 1 ;
-    int power ;
-    int counter ;
-    
-    
-    // Asks and scans for user input
-    
-    printf("Enter a non-zero real number: ");
-    scanf("%lf", &nonzero);
-    
-    printf("Enter an integer number for the power: ");
-    scanf("%i", &power);
-    
-    // Sets counter equal to 0. If counter is less than power add 1 to counter
-    
-    for(counter = 0; counter<power; counter++){
-        
-        // Sum equals sum multiplied by nonzero
-        
-        sum *= nonzero;
-    }
-    
-    // Prints results
-    
-    printf("sum = %lf", sum);
-    
-    return 0;
+int main() {
+
+	// Defines variables
+
+	double nonzero ;
+	double sum = 1 ;
+	int power ;
+	int counter ;
+
+
+	// Asks and scans for user input
+
+	printf("Enter a non-zero real number: ");
+	scanf("%lf", &nonzero);
+
+	printf("Enter an integer number for the power: ");
+	scanf("%i", &power);
+
+	// If power equals 0, sum equals 1
+
+	if(power == 0) {
+
+		sum = 1 ;
+	}
+
+	// If power is greater than 0
+
+	else if(power > 0) {
+
+		// Sets counter equal to 0. If counter is less than power add 1 to counter
+
+
+		for(counter = 0; counter<power; counter++) {
+
+			// Sum equals sum multiplied by nonzero
+
+			sum *= nonzero;
+		}
+	}
+
+	// If power is less than 0
+
+	else {
+
+		// Sets counter equal to 0.  If counter is greater than power subtract 1 from counter
+
+		for(counter = 0; counter>power; counter--) {
+
+			// Sum equals sum multiplied by (1 divided by nonzero)
+
+			sum *= (1/nonzero);
+
+		}
+	}
+
+	printf("sum = %lf", sum);
+
 }
