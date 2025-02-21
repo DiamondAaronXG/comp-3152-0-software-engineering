e545702024b1914c6dbfe21d5ad65ef5d252ca24
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-02-21 13:50:29 -0800

    Update minimum-of-two-numbers-function.c
    added comments

--- lab-6/minimum-of-two-numbers-function.c
+++ lab-6/minimum-of-two-numbers-function.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
+// Declares function
+
 float minVal(float num1, float num2) {
+
+	// If num1 is less than num2
+	
 	if(num1 < num2) {
+
+		// Returns num1
+		
 		return num1 ;
 	}
 
+	// If num1 is not less than num2
+
 	else {
+
+		// Returns num2
 
 		return num2 ;
 	}
@@ -13,7 +25,12 @@ float minVal(float num1, float num2) {
 }
 
 int main() {
+
+	// Declares variables
+	
 	float num1, num2, minValue;
+
+	// Asks and scans for user input
 
 	printf("Enter a number: ");
 	scanf("%f", &num1);
@@ -21,7 +38,13 @@ int main() {
 	printf("Enter a second number: ");
 	scanf("%f", &num2);
 
+	// Calls minValue function
+	
 	minValue = minVal(num1, num2);
+
+	// Prints results along wiht num1 and num2
+	
 	printf("The minimum value of %g and %g is %g. \n", num1,num2, minValue);
+	
 	return 0;
 }
