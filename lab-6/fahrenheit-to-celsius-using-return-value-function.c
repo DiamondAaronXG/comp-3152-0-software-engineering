45793278cc28532830e0dec1bb680384bf9a1d2c
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-02-21 13:35:55 -0800

    Update fahrenheit-to-celsius-using-return-value-function.c
    added comments

--- lab-6/fahrenheit-to-celsius-using-return-value-function.c
+++ lab-6/fahrenheit-to-celsius-using-return-value-function.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 
+// Declares function
+
 float temp(float F) {
+
+	// Returns (F-32)*(5/9)
 
 	return (F - 32) * (5 / 9);
 
 }
 int main() {
 
+	// Declares variables
+
 	float F ;
 	float C ;
 
+	// Asks and scans for user input
+
 	printf("Enter a Fahrenheit value:");
 	scanf("%f", &F);
+
+	// Calls temp function
+	
 	C = temp(F);
+
+	// Prints results
+	
 	printf("%f degrees in Celsius is %f degrees", F, C);
 
 }
