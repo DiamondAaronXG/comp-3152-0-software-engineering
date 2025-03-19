a3cee3bc425f43f2bf600efac6423da89b4a85da
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-03-19 13:44:31 -0700

    Update 2x2-matrix-and-inverse-matrix.cpp
    added comments
    allowed user input
    fixed error with boolean singular return

--- lab-7/2x2-matrix-and-inverse-matrix.cpp
+++ lab-7/2x2-matrix-and-inverse-matrix.cpp
@@ -3,15 +3,23 @@
 
 using namespace std ;
 
+// Sets class
+
 class Matrix {
 
 private:
+
+	// Declares variables
 
 	float a, b, c, d, detRes;
 
 public:
 
+	//Sets Constructor
+
 	Matrix(float a1, float b1, float c1, float d1) {
+
+		// Assigns values to variables passed from main
 
 		a = a1;
 		b = b1;
@@ -20,22 +28,36 @@ public:
 
 	}
 
+	// Sets determinent function
+
 	void det() {
 
+		// Formula to calculate determinant from matrix
+
 		detRes = (a*d - c*b) ;
+
+		// Prints results
 
 		cout << "|A| = " << detRes << endl;
 
 	}
 
+	// Sets function to find inverse matrix
+
 	void inverse() {
 
+		// Declares variables
+
 		float A00, A01, A10, A11;
+
+		// Formulas to calculate inverse matrix
 
 		A00 = d/(a*d - b*c);
 		A01 = -b/(a*d - b*c);
 		A10 = -c/(a*d - b*c);
 		A11 = a/(a*d - b*c);
+
+		// Prints results
 
 		cout << "A00 = " << A00 <<endl;
 		cout << "A01 = " << A01 <<endl;
@@ -44,13 +66,20 @@ public:
 
 	}
 
+	// Sets function to determine if matrix is singular
+
 	void isSingular() {
 
+		// If the matrix equals 0, output is 0
+
 		if(detRes == 0) {
-			cout << "Is singular? 0 "<< endl;
+			cout << "Is singular? 1 "<< endl;
 		}
+
+		// If the matrix is not equal to 0, output is 1
+
 		else {
-			cout << "Is singular?: 1" << endl;
+			cout << "Is singular?: 0" << endl;
 		}
 
 	}
@@ -59,7 +88,28 @@ public:
 
 int main() {
 
-	Matrix m( 1,  2,  3,  4);
+	// Declares variables
+
+	float a, b, c, d ;
+
+	cout << " Enter value 1 for the matrix: " << endl;
+	cin >> a ;
+
+	cout << " Enter value 2 for the matrix: " << endl;
+	cin >> b ;
+
+	cout << " Enter value 3 for the matrix: " << endl;
+	cin >> c ;
+
+	cout << " Enter value 4 for the matrix: " << endl;
+	cin >> d ;
+
+	// Calls the Matrix class and passes variables
+
+	Matrix m( a,  b,  c,  d);
+
+	// Prints results
+
 	m.det();
 	m.inverse();
 	m.isSingular();
@@ -67,4 +117,3 @@ int main() {
 	return 0;
 
 }
-
