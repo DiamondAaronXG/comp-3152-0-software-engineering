73adfec117cfed84e25a24a0b743fbd1cff7bbc6
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-03-20 10:58:28 -0700

    Update mylib.c

--- lab-8/mylib.c
+++ lab-8/mylib.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 
+// Declares function to add two resistors in series
+
 float rSer(float R1,float R2) {
 
 	return (R1 + R2);
 
 }
+
+// Declared\s function to add two resistors in parallel
 
 float rPar(float R1,float R2) {
 
@@ -13,17 +17,23 @@ float rPar(float R1,float R2) {
 
 }
 
+// Declared\s function to add two capacitors in series
+
 float cSer(float C1,float C2) {
 
 	return (C1 * C2) / (C1 + C2);
 
 }
 
+// Declares function to add two capacitors in parallel
+
 float cPar(float C1,float C2) {
 
 	return (C1 + C2);
 
 }
+
+// Declares function to find distance between two  sets of x,y points
 
 float getDis(float x1,float y1,float x2,float y2) {
 
