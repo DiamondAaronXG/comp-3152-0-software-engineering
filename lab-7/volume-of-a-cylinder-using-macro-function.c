d914c11e414194af29e55488630068985f4bddc5
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-03-20 10:52:25 -0700

    Update volume-of-a-cylinder-using-macro-function.c

--- lab-7/volume-of-a-cylinder-using-macro-function.c
+++ lab-7/volume-of-a-cylinder-using-macro-function.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #define PI 3.14159
+
+// Defines macro function
+
 #define V(r,h) (PI * r * r * h)
+
 int main()
 {
+	// Declares variables
+	
 	float r=2, h=1.5 ;
+
+	// Prints results
 
 	printf("\n Volume of a cylinder = %g", V(r,h));
 
