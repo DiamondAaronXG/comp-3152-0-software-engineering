477b37fc7ef4bbd1c3e0f72281f566e479482b0e
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-03-20 09:48:43 -0700

    Create mylib.h

---
+++ lab-8/mylib.h
@@ -0,0 +1,9 @@
+#define PI 3.14159
+
+#define getPerimeter(r) (PI * r * r)
+
+float rSer(float R1,float R2);
+float rPar(float R1,float R2);
+float cSer(float C1,float C2);
+float cPar(float C1,float C2);
+float getDis(float x1,float y1,float x2,float y2);
