662f58830851a1ac12813699463f8a4829a28ed9
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-03-20 10:51:41 -0700

    Update and rename area-of-a-sphere-using-macro-function.c to area-of-a-sphere-macro-function.c
    concised name
    added comments

--- lab-7/area-of-a-sphere-macro-function.c
+++ lab-7/area-of-a-sphere-macro-function.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #define PI 3.14159
+
+// Defines macro function
+
 #define area(r) (4 * PI * r * r)
+
 int main()
 {
+	// Declares variable
+	
 	float r=1.5 ;
+
+	// Prints results
 
 	printf("\n Area of a sphere = %g", area(r));
 
