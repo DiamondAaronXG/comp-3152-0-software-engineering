78d6b69e671410b9bce30ac7ada0bed6a5cb2f92
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-03-27 09:40:01 -0700

    Create largest-number-in-an-array.c

---
+++ lab-9/largest-number-in-an-array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float A[] = {2, 1.2, -4, 6, 3.2} ;
+    int x , n = sizeof(A) / sizeof(A[0]); 
+    float sum = 0 ;
+    
+    for(x=0; x<n; x++){
+        sum += A[x] ;
+    }
+    
+    
+    
+    printf("sum = %f", sum);
+
+    return 0;
+}
