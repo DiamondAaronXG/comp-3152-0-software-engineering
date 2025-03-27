057a21f2f502a8c716873d70349d1fcec4d6d7be
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-03-27 16:17:28 -0700

    Update number-of-chosen-character-in-a-chosen-string.c
    added comments
    "beautified"

--- lab-9/number-of-chosen-character-in-a-chosen-string.c
+++ lab-9/number-of-chosen-character-in-a-chosen-string.c
@@ -2,26 +2,28 @@
 
 int main() {
 
-	char st1[100], c ;
+	// Initializes variables and string
 
+	char st1[100], c ;
 	int counter = 0, i ;
 
-	printf("Enter a string: ");
+	// Asks for and scans user input for string and character to check repitition of
 
+	printf("Enter a string: ");
 	scanf("%s", st1 );
 
 	printf("Enter a character: ");
-
 	scanf(" %c", &c);
 
+	// Checks how many times the chosen character appears in the string
+
 	for(i = 0; st1[i] != '\0'; i++) {
-
 		if(st1[i] == c) {
-
 			counter++ ;
 		}
-
 	}
+
+	// Prints results
 
 	printf("# of repeated %c's is %d", c, counter);
 
