95dbe69252031c9c3cae102897a26c56cabd25e5
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-03-27 16:15:15 -0700

    Update upper-to-lower-and-lower-to-upper.c
    added ability for user input
    added comments

--- lab-9/upper-to-lower-and-lower-to-upper.c
+++ lab-9/upper-to-lower-and-lower-to-upper.c
@@ -3,24 +3,41 @@
 #include <string.h>
 
 int main(void) {
-    char st1[] = "HeLlO wOrLd" ;
-    int x=0 ;
-    
-     printf("Original String: %s \n", st1);
-     
-     while(st1[x]){
-         if(islower(st1[x])){
-             st1[x] = toupper(st1[x]) ;
-         }
-         
-         else if(isupper(st1[x])){
-             st1[x] = tolower(st1[x]) ;
-         }
-         
-         x++ ;
-     }
-    
-    printf("Modified String = %s", st1) ;
-    
-    return 0 ;
+
+	// Initializes variables and string
+
+	char st1[100] ;
+	int x=0 ;
+
+	// Asks for and saves user input for string
+
+	printf("Enter a string: ");
+	fgets(st1, sizeof(st1), stdin);
+
+	// Prints inputed string
+
+	printf("Original String: %s \n", st1);
+
+	while(st1[x]) {
+
+		//Changes all lower case to upper case withing string
+
+		if(islower(st1[x])) {
+			st1[x] = toupper(st1[x]) ;
+		}
+
+		//Changes all upper case to lower case within string
+
+		else if(isupper(st1[x])) {
+			st1[x] = tolower(st1[x]) ;
+		}
+
+		x++ ;
+	}
+
+	// Prints results (modified string)
+
+	printf("Modified String = %s", st1) ;
+
+	return 0 ;
 }
