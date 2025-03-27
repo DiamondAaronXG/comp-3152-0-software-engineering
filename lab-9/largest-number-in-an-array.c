70ad94433a5cc0543394b24e4b0c1cf68b1e0d05
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-03-27 09:42:29 -0700

    Update largest-number-in-an-array.c
    uploaded correct code

--- lab-9/largest-number-in-an-array.c
+++ lab-9/largest-number-in-an-array.c
@@ -2,17 +2,20 @@
 
 int main()
 {
-    float A[] = {2, 1.2, -4, 6, 3.2} ;
-    int x , n = sizeof(A) / sizeof(A[0]); 
+    float A[5] = {2, 9.3, -4, 6, 3.2} ;
+    int x ;
     float sum = 0 ;
     
-    for(x=0; x<n; x++){
-        sum += A[x] ;
+    for(x=0; x<5; x++){
+        if(sum > A[x]){
+            sum = sum ;
+        }
+            
+        else{
+            sum = A[x] ;
+        }
     }
-    
-    
-    
-    printf("sum = %f", sum);
 
+    printf("sum = %f" , sum) ;
     return 0;
 }
