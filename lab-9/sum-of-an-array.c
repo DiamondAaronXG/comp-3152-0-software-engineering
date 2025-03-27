f952f40d19171a10c3120ffae5658007a5e5b1ba
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-03-27 16:03:12 -0700

    Update sum-of-an-array.c
    added ability for user input
    added comments

--- lab-9/sum-of-an-array.c
+++ lab-9/sum-of-an-array.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 
-int main()
-{
-    float A[] = {2, 1.2, -4, 6, 3.2} ;
-    int x , n = sizeof(A) / sizeof(A[0]); 
-    float sum = 0 ;
-    
-    for(x=0; x<n; x++){
-        sum += A[x] ;
-    }
-    
-    printf("sum = %f", sum);
+int main() {
 
-    return 0;
+	// Initializes variables
+
+	int t, x, i ;
+	float sum = 0 ;
+
+	// Asks for and scans user input for number of array elements
+
+	printf("Enter the number of elements in the array: ");
+	scanf("%d", &t);
+
+	// Initializes array
+
+	float arr[t] ;
+
+	// Asks for and scans user input for array elements
+
+	printf("Enter numbers for the array (Click enter after each entry): \n");
+	for(i=0; i<t; i++) {
+		scanf("%f", &arr[i]);
+	}
+
+	// Calculates the sum of all elements in the array
+
+	for(x=0; x<t; x++) {
+		sum += arr[x] ;
+	}
+
+	// Prints results
+
+	printf("sum = %f", sum);
+
+	return 0;
 }
