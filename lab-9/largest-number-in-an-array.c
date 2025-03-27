46f2270017ab1babdc199efb55d330cc3d162532
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-03-27 15:53:02 -0700

    Update largest-number-in-an-array.c
    added ability for user input
    added comments

--- lab-9/largest-number-in-an-array.c
+++ lab-9/largest-number-in-an-array.c
@@ -1,21 +1,38 @@
 #include <stdio.h>
 
-int main()
-{
-    float A[5] = {2, 9.3, -4, 6, 3.2} ;
-    int x ;
-    float sum = 0 ;
-    
-    for(x=0; x<5; x++){
-        if(sum > A[x]){
-            sum = sum ;
-        }
-            
-        else{
-            sum = A[x] ;
-        }
-    }
+int main() {
 
-    printf("sum = %f" , sum) ;
-    return 0;
+	// Initializes variables
+
+	int x, t, i ;
+	float max = 0 ;
+
+	// Asks for and scans user input for number of elements in the array
+
+	printf("Enter number of elements in the array: ");
+	scanf("%d", &t);
+
+	// Initializes array
+
+	float arr[t] ;
+
+	// Asks for and scans user input for array elements
+
+	printf("Enter numbers for the array (Click enter after each entry): \n");
+	for(i=0; i<t; i++) {
+		scanf("%f", &arr[i]);
+	}
+
+	// Determines which array element is the largest
+
+	for(x=0; x<t; x++) {
+		if(max < arr[x]) {
+			max = arr[x] ;
+		}
+	}
+
+	// Prints results
+
+	printf("max = %f", max);
+	return 0;
 }
